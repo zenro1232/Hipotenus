f747586ea5744f38b3371576e2cc2069691868f3
zenro1232 <126719730+zenro1232@users.noreply.github.com>
2023-05-25 17:47:42 +0300

    Add files via upload

---
+++ src/üçgen.java
@@ -0,0 +1,23 @@
+
+Taksimetre.java
+import java.util.Scanner;
+public class Taksimetre
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int km;
+        double perkm = 2.20, total = 10, min = 20;
+        System.out.print("Lütfen KM cinsinden giriniz");
+        km = input.nextInt();
+
+        total += (perkm * km);
+
+        total = total < 20 ? 20 : total;
+
+        System.out.print(total);
+
+
+
+
+    }
+}
